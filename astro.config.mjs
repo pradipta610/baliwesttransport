c6d619ec4c8f67972f85c3fc21ff83be5447309a
pradipta610 <pradipta.wistika@gmail.com>
2026-06-10 19:41:30 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 export default defineConfig({
   site: 'https://baliwesttransport.com',
+  trailingSlash: 'always',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'en',
